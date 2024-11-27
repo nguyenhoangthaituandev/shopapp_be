101e9f140361412ae80fcfa6301cdc5db00b4072
nguyenhoangthaituandev <nguyenhoangthaituandev@gmail.com>
2024-11-27 22:06:45 +0700

    base project, controller validation,

---
+++ src/main/java/com/company/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package com.company.controller;
+
+import com.company.forms.CategoryForm;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/categories")
+//@Validated
+public class CategoryController {
+    @GetMapping
+    public ResponseEntity<String> GetAllCategories(
+            @RequestParam("page") int page,
+            @RequestParam("limit") int limit
+    ){
+        return ResponseEntity.ok(String.format("Get All categories here with page = %d limit = %d",page,limit));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createCategory(
+            @RequestBody @Valid CategoryForm categoryForm,
+            BindingResult result){
+        if(result.hasErrors()){
+           List<String> errorMessages =result.getFieldErrors()
+                    .stream()
+                    .map(FieldError:: getDefaultMessage)
+                    .toList();
+           return ResponseEntity.badRequest().body(errorMessages);
+        }
+        return ResponseEntity.ok("Create category "+categoryForm);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCategory(@PathVariable(name = "id") Long categoryID){
+        return ResponseEntity.ok("Update category: "+categoryID);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable(name = "id") Long categoryID){
+        return ResponseEntity.ok("Delete category: "+categoryID);
+    }
+}
---
+++ src/main/java/com/company/forms/CategoryForm.java
@@ -0,0 +1,15 @@
+package com.company.forms;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CategoryForm {
+    @NotEmpty(message="Category can not be empty")
+    private String name;
+}
