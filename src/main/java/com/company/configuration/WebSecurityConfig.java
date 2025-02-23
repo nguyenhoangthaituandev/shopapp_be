7a020c40945d4bc8dfac2cea0d596f4dbb122ff2
nguyenhoangthaituandev <nguyenhoangthaituandev@gmail.com>
2025-02-23 19:24:35 +0700

    [add] doFilterBefore, JwtTokenFilter

--- src/main/java/com/company/configuration/WebSecurityConfig.java
+++ src/main/java/com/company/configuration/WebSecurityConfig.java
@@ -1,23 +1,30 @@
 package com.company.configuration;
 
+import com.company.filters.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class WebSecurityConfig {
+    private final JwtTokenFilter jwtTokenFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
          http
                 .csrf(AbstractHttpConfigurer::disable)
+                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
                  .authorizeHttpRequests(request->{
-                     request.requestMatchers("**").permitAll();
+                     request.requestMatchers("**")
+                             .permitAll();
                  });
 
          return http.build();
